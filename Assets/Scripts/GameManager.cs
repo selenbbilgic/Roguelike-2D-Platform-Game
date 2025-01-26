caf7d9328cc3bde657befe3da7a7c94d6c4612be
selenbbilgic <sbilgic20@ku.edu.tr>
2025-01-26 22:57:36 +0300

    food count label is added.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
-   public BoardManager BoardManager;
+    public BoardManager BoardManager;
     public PlayerController PlayerController;
 
-   public TurnManager TurnManager { get; private set;}
-    
+    public TurnManager TurnManager { get; private set;}
+    private int m_FoodAmount = 100;
     private void Awake()
    {
        if (Instance != null)
@@ -23,14 +23,14 @@ public class GameManager : MonoBehaviour
    void Start()
    {
        TurnManager = new TurnManager();
-      
+        TurnManager.OnTick += OnTurnHappen;
        BoardManager.Init();
        PlayerController.Spawn(BoardManager, new Vector2Int(1,1));
    }
 
-    // Update is called once per frame
-    void Update()
+    void OnTurnHappen()
     {
-        
+    m_FoodAmount -= 1;
+    Debug.Log("Current amount of food : " + m_FoodAmount);
     }
 }
